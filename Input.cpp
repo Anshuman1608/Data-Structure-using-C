b98124d37d7dca46d4d96efa3018b46711e6277f
Anshuman Singh <anshumaansingh98@gmail.com>
2021-05-21 13:56:15 +0530

    Create Input.cpp

---
+++ Input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int amount1;    //intialization of first variable
+    cin>>amount1;   //Input from user 1
+
+    int amount2;    //intialization of second variable
+    cin>>amount2;   //Input from user 2
+
+    int sum= amount1+amount2;   //Calcualtion
+    cout<<sum;          //Output
+    return 0;
+}
